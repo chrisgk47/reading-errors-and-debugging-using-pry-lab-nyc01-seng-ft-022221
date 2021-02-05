adfcc772b407fae7c60976d2872ce76da28421bb
Christopher Kim <chris.kim1990@gmail.com>
2021-02-05 23:39:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,9 +3,9 @@ def snake_it_up(string)
   string = "surprise!"
   if string[0] == "s"
     10.times{string[0]} + string
-  else
     binding.pry
-    string
     "hello"
+  else
+    string
   end
 end
