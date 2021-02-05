231d0cd6760f32c06fe35dac007c33fe328086bc
Christopher Kim <chris.kim1990@gmail.com>
2021-02-05 23:38:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,7 +2,7 @@ require 'pry'
 def snake_it_up(string)
   string = string.to_i
   if string[0] == "s"
-    10.times{"s"} + string
+    10.times{string[0]} + string
   else
     binding.pry
     string
